269e3fa726edcbca2ca5ae6cb1932721ae82fac3
emenym <emenym@gmail.com>
2019-09-04 14:47:17 -0400

    basic ajax working

--- static/list.js
+++ static/list.js
@@ -14,22 +14,44 @@ $(document).ready(function(){
 
     });
 
-    $('#getgroc').click(function() {
-        list = $('#list')
-        if (list.children().length > 0){
-            $('.list-group').append("<li class='list-group-item'>"+input.val()+"</li>");
-            // clear input and typehead
-            input.val('')
-                    $('.typeahead').typeahead('val', '');
-        }
-
-    });
+//    $('#getgroc').click(function(gifurl) {
+////         $('#getgroc').html('<img src="{{ url_for('static', filename='loading.gif') }}">');
+////        $('#getgroc').html('<img src ='+gifurl+'>');
+//        list = $('#list');
+//        if (list.children().length > 0){
+//            $('.list-group').append("<li class='list-group-item'>"+input.val()+"</li>");
+//            // clear input and typehead
+//            input.val('');
+//            $('.typeahead').typeahead('val', '');
+//        }
+//
+//    });
 
     $('#userinput').on('keyup', function (e) {
     if (e.keyCode === 13) {
         $('#add').click();
     }
-});
+    });
 
 
+
 });
+
+function get_groceries(gifurl) {
+    $('#getgroc').html('<img src ='+gifurl+'>');
+    recipelist = [];
+    $( "#list li"  ).each(function( index ) {
+        console.log( index + ": " + $( this ).text() );
+        recipelist.push($( this ).text())
+    });
+
+
+    $.ajax({
+    url: "/get_groceries",
+    type: "POST",
+    data: JSON.stringify({recipes: recipelist}),
+    contentType: "application/json; charset=utf-8",
+    success: function(dat) { console.log(dat); }
+});
+
+}
