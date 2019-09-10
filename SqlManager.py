4fdfe238900ce15c4698407c08fa90b531ba9614
emenym <emenym@gmail.com>
2019-09-10 13:43:18 -0400

    hook up to real database

---
+++ SqlManager.py
@@ -0,0 +1,81 @@
+import mysql.connector
+import os
+
+
+def connect():
+    conn = mysql.connector.connect(
+        host="ns2.usm26.siteground.biz",
+        user=os.environ.get("MYSQL_USER"),
+        passwd=os.environ.get("MYSQL_PWD"),
+        database=os.environ.get("MYSQL_DB")
+    )
+    return conn
+
+
+def close(conn):
+    conn.close()
+
+
+def get_recipe_list():
+    query = 'SELECT RecipeName FROM RecipeMaster'
+    result = execute_query(query)
+    return result
+
+
+def get_groceries(recipe_id):
+    query = 'SELECT GROCERIES.GroceryName, ' \
+            "RecipeDetail.Qty, " \
+            "UOM_Tbl.Descr " \
+            "FROM RecipeDetail " \
+            "INNER JOIN GROCERIES " \
+            "ON RecipeDetail.GroceryNameID = GROCERIES.GroceryNameID " \
+            "INNER JOIN UOM_Tbl " \
+            "ON RecipeDetail.UOM_ID = UOM_Tbl.ID " \
+            "WHERE RecipeMasterID = %s "\
+            "AND UOM_Tbl.Descr != %s"
+
+    result = execute_query(query, [recipe_id, "CommentLine"])
+    print_result(result)
+    return result
+
+
+def get_recipe_id(recipe):
+    query = "SELECT ID FROM RecipeMaster WHERE RecipeName =  %s"
+    result = execute_query(query, [recipe])
+    return result[0][0]
+
+
+def print_result(res):
+    for x in res:
+        print(x)
+
+
+def execute_query(query, args=None):
+    db = connect()
+    cursor = db.cursor()
+    if args:
+        parameters = tuple(args)
+        cursor.execute(query, parameters)
+    else:
+        cursor.execute(query)
+    result = cursor.fetchall()
+    cursor.close()
+    close(db)
+    return result
+
+
+def print_schema(table):
+    query = 'describe '+table
+    result = execute_query(query)
+    print(table+' Schema')
+    for x in result:
+        print(x)
+
+
+if __name__ == '__main__':
+    tables = execute_query('show tables')
+    print(tables)
+    print_schema('UOM_Tbl')
+    id = get_recipe_id('Chili')
+    grocs = get_groceries(id)
+    execute_query('select * from RecipeMaster')
--- planner.py
+++ planner.py
@@ -1,49 +1,39 @@
-
 import os
-import random
-import mysql.connector
 from flask import Flask, request, jsonify, abort
 from flask import render_template
-
+import pint
+import SqlManager
 
 
 app = Flask(__name__)
+UREG = pint.UnitRegistry()
 
 
 @app.route("/")
 def planner():
-    # select RecipeName from RecipeMaster
-    recipe_list = ['Chili',
-                   'Pizza, Homemade',
-                   'Sausage Seasoning',
-                   'Sausage,Hot Italian  Homemade',
-                   'Sauce, Spagetti',
-                   'Taco Meat; Homemade',
-                   'Chili Powder, Homemade',
-                   'Pizza Dough',
-                   'Pancakes,Cornmeal',
-                   'Hamburg and Gravy']
+    recipe_list = get_recipe_list()
     return render_template("planner.html",
                            recipes=recipe_list)
+
+
+def get_recipe_list():
+    recipes = SqlManager.get_recipe_list()
+    return recipes
 
 
 @app.route('/get_groceries', methods=['POST'])
 def get_groceries():
     data = request.get_json()
-    # if len(data['recipes']) < 1:
-    #
-    #     return {}
     groceries = process_recipes(data['recipes'])
-
     return jsonify(groceries=groceries)
 
 
 def process_recipes(recipes):
     grocery_list = {}
     for recipe in recipes:
-        # grocery = get_ingredients(recipe)
-        grocery = get_temp_ingredients(recipe)
-        grocery_list.update(grocery)
+        recipe_id = SqlManager.get_recipe_id(recipe)
+        grocery = SqlManager.get_groceries(recipe_id)
+        grocery_list.update({recipe: grocery})
 
     return condense(grocery_list)
 
@@ -53,77 +43,36 @@ def condense(grocery_list):
     grocery_list_condensed = {}
     for groc in grocery_list:
         for item in grocery_list[groc]:
-            if item['name'] in grocery_list_condensed.keys():
-                grocery_list_condensed[item['name']]['qty'] += item['qty']
-
+            name = item[0]
+            qty = item[1]
+            unit = item[2]
+            if name in grocery_list_condensed.keys():
+                q1 = {'qty': grocery_list_condensed[name]['qty'],
+                      'unit': grocery_list_condensed[name]['unit']
+                      }
+                q2 = {'qty': qty, 'unit': unit}
+                grocery_list_condensed[name] = add_quantities(q1, q2)
             else:
-                # grocery_list_condensed.update(item)
-                grocery_list_condensed[item['name']] = {'qty': item['qty'], 'unit': item['unit']}
+                grocery_list_condensed[name] = {'qty': qty, 'unit': unit}
     return grocery_list_condensed
 
 
-def get_temp_ingredients(recipe):
-    list = ['broccoli', 'eggs', 'butter', "cheese", "flour",
-            "apples", "lettuce", 'wine', 'grocery with really long name wow']
-
-    list2 = [1, 2, 3, 4, 5]
-    list3 = ['cups', 'teaspoons', 'ounces']
-    recipestuff = {recipe: []}
-
-    ingredients = []
-    for i in range(0, random.choice([2, 3, 4])):
-        grocerystuff = {"name": list.pop(list.index(random.choice(list))),
-                        "qty": random.choice(list2),
-                        "unit": random.choice(list3)}
-        ingredients.append(grocerystuff)
-
-    for grocery in ingredients:
-        recipestuff[recipe].append(grocery)
-    return recipestuff
-
-
-def get_ingredients(recipe):
-    # select name from ingredientmaster where
-    conn = mysql.connector.connect(
-        user=os.environ.get("MYSQL_USER"),
-        passwd=os.environ.get("MYSQL_PWD"),
-        database=os.environ.get("MYSQL_DB"))
-    cursor = conn.cursor()
-
-    query = ("SELECT first_name, last_name, hire_date FROM employees "
-             "WHERE hire_date BETWEEN %s AND %s")
-
-    # do some gnarly query to get unique grocery names
-    query = ("SELECT name where recepe IS %s JOIN A BUNCH OF SHIT")
-
-    # from kdc
-    sql = "SELECT GroceryNameId ID, GroceryName Name, gramsPerCup FROM GROCERIES "
-    sql += "UNION SELECT ID, RecipeName Name, gramsPerCup FROM RecipeMaster "
-    sql += "WHERE isSubRecipe=1 ORDER BY Name"
-
-    sql = "SELECT Qty, UOM_ID, GroceryNameID, Instruction, ID AS detailID, Sequence AS seq "
-    sql += "FROM RecipeDetail "
-    sql += "WHERE RecipeMasterID = ? "
-    sql += "ORDER BY Sequence"
-
-    cursor.execute(query, recipe)
-
-    for (GroceryName, Qty) in cursor:
-        print("{} times {}".format(
-            GroceryName, Qty))
-
-    cursor.close()
-    conn.close()
-    return ''
+def add_quantities(q1, q2):
+    thing1 = q1['qty'] * UREG.parse_expression(q1['unit'].lower())
+    thing2 = q2['qty'] * UREG.parse_expression(q2['unit'].lower())
+    thing3 = thing1 + thing2
+    result = {'qty': thing3.magnitude, 'unit': str(thing3.units).capitalize()}
+    return result
 
 
 def pretty(d, indent=0):
-   for key, value in d.items():
-      print('\t' * indent + str(key))
-      if isinstance(value, dict):
-         pretty(value, indent+1)
-      else:
-         print('\t' * (indent+1) + str(value))
+    for key, value in d.items():
+        print('\t' * indent + str(key))
+        if isinstance(value, dict):
+            pretty(value, indent+1)
+        else:
+            print('\t' * (indent+1) + str(value))
+
 
 if __name__ == "__main__":
     # asd = get_temp_ingredients('lol')
